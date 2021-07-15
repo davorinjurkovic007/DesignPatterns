263263b0ce66711fb0ef97b98d972a97b8e98330
Davorin <davorin.jurkovic@gmail.com>
2021-07-15 21:44:45 +0200

    Observer Pattern - begining

---
+++ Observer.MotivatingExample/Observer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			foreach (Stock s in SampleData.getNext())
+			{
+				// Reactive Filters
+				if (s.Symbol == "GOOG")
+					Console.WriteLine("Google's new price is: {0}", s.Price);
+
+				if (s.Symbol == "MSFT" && s.Price > 10.00m)
+					Console.WriteLine("Microsoft has reached the target price: {0}", s.Price);
+
+				if (s.Symbol == "XOM")
+					Console.WriteLine("Exxon mobile's price is {0}", s.Price);
+			}
+		}
+    }
+}
