f916b655c5ec8d7cf94a152fe0814eee4f077840
Davorin <davorin.jurkovic@gmail.com>
2021-09-10 00:10:36 +0200

    Naive approach

---
+++ Singleton/Singleton.Test/V1_Naive/SingletonInstance.cs
@@ -0,0 +1,80 @@
+﻿using Singleton.V1_Naive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true)]
+
+namespace Singleton.Test.V1_Naive
+{
+    public class SingletonInstance
+    {
+        private readonly ITestOutputHelper _output;
+        public SingletonInstance(ITestOutputHelper output)
+        {
+            _output = output;
+            SingletonTestHelpers.Reset(typeof(SingletonNaive));
+            Logger.Clear();
+        }
+
+        [Fact]
+        public void ReturnsNonNullSingletonInstance()
+        {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance<SingletonNaive>());
+
+            var result = SingletonNaive.Instance;
+
+            Assert.NotNull(result);
+            Assert.IsType<SingletonNaive>(result);
+
+            Logger.Output().ToList().ForEach(h => _output.WriteLine(h));
+        }
+
+        [Fact]
+        public void OnlyCallsConstructorOnceGivenInstanceCalls()
+        {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance<SingletonNaive>());
+
+            var one = SingletonNaive.Instance;
+            var two = SingletonNaive.Instance;
+            var three = SingletonNaive.Instance;
+
+            var log = Logger.Output();
+            Assert.Equal(1, log.Count(log => log.Contains("Constructor")));
+            Assert.Equal(3, log.Count(log => log.Contains("Instance")));
+
+            Logger.Output().ToList().ForEach(h => _output.WriteLine(h));
+        }
+
+        [Fact]
+        public void CallsConstructorMultipleTimesGivenThereeParallelInstanceCalls()
+        {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance<SingletonNaive>());
+
+            // configure logget to slow down the cration long enough to couse problems
+            Logger.DelayMiliseconds = 5000;
+
+            var strings = new List<string>() { "one", "two", "three" };
+            var instances = new List<SingletonNaive>();
+            var options = new ParallelOptions() { MaxDegreeOfParallelism = 3 };
+            Parallel.ForEach(strings, options, instance =>
+            {
+                instances.Add(SingletonNaive.Instance);
+            });
+
+            var log = Logger.Output();
+            try
+            {
+                Assert.True(log.Count(log => log.Contains("Constructor")) > 1);
+                Assert.Equal(3, log.Count(log => log.Contains("Instance")));
+            }
+            finally
+            {
+                Logger.Output().ToList().ForEach(h => _output.WriteLine(h));
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton/V1_Naive/SingletonNaive.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton.V1_Naive
+{
+    // Bad code
+#nullable enable
+
+    public sealed class SingletonNaive
+    {
+        private static SingletonNaive? _instance;
+
+        public static SingletonNaive Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                return _instance ??= new SingletonNaive();
+            }
+        }
+
+        private SingletonNaive()
+        {
+            // cannot be created except withing this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
